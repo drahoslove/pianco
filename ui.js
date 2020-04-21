322d73564478ce0ace6d731bf99374e50b39f370
Draho <drahoslav@yo2.cz>
2020-04-21 02:53:58 +0200

    C6 to note stuff

--- ui.js
+++ ui.js
@@ -98,6 +98,7 @@ const wholeNote = [...staffBox.children].pop()
 ;[
   'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4',
   'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5',
+  'C6',
 ].forEach((ch, i) => {
   const hspace = 0.2
   const hoffset = 3 // .6
