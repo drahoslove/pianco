3ffea16b3fb74ed04bc53c9109c47ece974acab0
Draho <drahoslav@yo2.cz>
2020-04-23 00:14:24 +0200

    Use cert on backend if given

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 11088 })
- 
+const WebSocket = require('ws')
+
+const PORT = 11088
+const { SSL_KEY, SSL_CA, SSL_CERT } = process.env
+
+let server
+if (!SSL_KEY || !SSL_CA || !SSL_CERT) { // http
+	server = require('http').createServer().listen(PORT)
+} else { // https
+	let fs = require('fs')
+	server = require('https').createServer({
+		key: fs.readFileSync(SSL_KEY),
+		cert: fs.readFileSync(SSL_CERT),
+		ca: fs.readFileSync(SSL_CA),
+	}).listen(PORT)
+}
+
+const wss = new WebSocket.Server({ server })
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     wss.clients.forEach(client => {
@@ -12,3 +28,4 @@ wss.on('connection', function connection(ws) {
   console.log('client connected')
   ws.send('connected')
 })
+
