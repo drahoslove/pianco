bee8c9e25c3c6023104110fc1e3a4154f9b37f33
Draho <drahoslav@yo2.cz>
2021-01-29 06:13:37 +0100

    Update connect-midi script to conect gopiano service

--- raspberrypi/connect-midi.sh
+++ raspberrypi/connect-midi.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# put into /usr/local/bin/
+
 # `aconnect -l`
 # to get right values
+
+# connect tablet with piano
 aconnect 24:0 20:0
 aconnect 20:0 24:0
+# connect piano with gopiano service
+aconnect 20:0 129:0
