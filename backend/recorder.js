0f327c2446d6bf0fe28614c69f1ec5c1951cb42b
Draho <drahoslav@yo2.cz>
2021-02-13 19:01:15 +0100

    Improve behavir of recorder when recording after pause

--- backend/recorder.js
+++ backend/recorder.js
@@ -1,6 +1,28 @@
 const { performance } = require('perf_hooks')
 
-const { fromCmd, toCmd, toVal, CC_SUTAIN, NOTE_A0, NOTE_C8 } = require('./midi.js')
+const {
+  fromCmd, toCmd, toVal,
+  CC_SUTAIN, NOTE_A0, NOTE_C8,
+} = require('./midi.js')
+
+let WSS // set on importy
+
+
+// stopped -> recording
+// recording -> stopped
+// recording -> playing
+// stopped -> playing
+// playing -> stopped (auto)
+// paying -> paused
+// paused -> playing
+// paused !x! recording
+
+//            seekTime      isRecording     isPlaying
+// stopped    0             false           false
+// recording  ?             true            false
+// playing    >0            false           true
+// paused     >0            false           false
+
 
 class Tape {
   isRecording = false
@@ -14,59 +36,64 @@ class Tape {
     this.uid = uid
   }
   record () {
-    this.tape = this.tape.slice(0,this.seekIndex)
     this.isRecording = true
-    this.startTime = this.tape.length > 0
-      ? performance.now() - this.tape[this.tape.length-1].time
-      : performance.now()
-    Recorder.wss.send(this.gid, this.uid, `isRecording ${true}`)
+    WSS.send(this.gid, this.uid, `isRecording ${true}`)
   }
   stop () {
     this.isRecording = false
-    Recorder.wss.send(this.gid, this.uid, `isRecording ${false}`)
     this.isPlaying = false
-    Recorder.wss.send(this.gid, this.uid, `isPlaying ${false}`)
+    WSS.send(this.gid, this.uid, `isRecording ${false}`)
+    WSS.send(this.gid, this.uid, `isPlaying ${false}`)
     this.seekTime = 0
     this.seekIndex = 0
     clearTimeout(this.timeout)
-    this.timeout = 0
   }
   play () {
     if (this.isRecording) {
+      this.seekIndex = 0
+      this.seekTime = 0
       this.isRecording = false
-      Recorder.wss.send(this.gid, this.uid, `isRecording ${false}`)
+      WSS.send(this.gid, this.uid, `isRecording ${false}`)
     }
     this.isPlaying = true
-    Recorder.wss.send(this.gid, this.uid, `isPlaying ${true}`)
+    WSS.send(this.gid, this.uid, `isPlaying ${true}`)
     const event = this.tape[this.seekIndex]
-    if (event) { // schedule replay of event
-      this.timeout = setTimeout(() => {
-        this.seekIndex++
-        this.seekTime = event.time
-        Recorder.wss.broadcast(event.data)
-        this.play() // next note
-      }, event.time - this.seekTime)
-    } else {
+    if (!event) {
       this.stop()
+      return
     }
+    // schedule replay of event
+    this.timeout = setTimeout(() => {
+      this.seekIndex++
+      this.seekTime = event.time
+      WSS.broadcast(event.data)
+      this.play() // next note
+    }, event.time - this.seekTime)
   }
   pause () {
     this.isPlaying = false
-    this.isRecording = false
     clearTimeout(this.timeout)
-    Recorder.wss.send(this.gid, this.uid, `isPlaying ${false}`)
+    WSS.send(this.gid, this.uid, `isPlaying ${false}`)
   }
   write (data) {
     if (!this.isRecording) {
       return
     }
-    if (this.tape.length === 0) {
+    if (this.seekIndex < this.tape.length) { // chop off rest of the tape
+      this.tape.length = this.seekIndex
+      if (this.tape.length > 0) { // adjust depending on the last times
+        this.startTime = performance.now() - this.tape[this.tape.length-1].time
+      }
+    }
+    if (this.tape.length === 0) { // leave fixed space at start
       this.startTime = performance.now() - 250
     }
+
     // change channel in data
     data[2] = toCmd(fromCmd(data[2]))
     const time = performance.now() - this.startTime
     this.tape.push({ time, data })
+    this.seekIndex = this.tape.length
   }
 }
 
@@ -105,20 +132,20 @@ class Recorder {
   }
 
   sendSustain = (value, uid) => {
-    Recorder.wss.broadcast([this.gid, uid, toCmd(3), CC_SUTAIN, toVal(value)])
+    WSS.broadcast([this.gid, uid, toCmd(3), CC_SUTAIN, toVal(value)])
   }
   
   sendNoteOn = (note, uid) => {
-    Recorder.wss.broadcast([this.gid, uid, toCmd(1), note, toVal(note.velocity)])
+    WSS.broadcast([this.gid, uid, toCmd(1), note, toVal(note.velocity)])
   }
   
   sendNoteOff = (note, uid) => {
-    Recorder.wss.broadcast([this.gid, uid, toCmd(0), note, toVal(0)])
+    WSS.broadcast([this.gid, uid, toCmd(0), note, toVal(0)])
   }
 }
 
 
 module.exports = (server) => {
-  Recorder.wss = server
+  WSS = server
   return Recorder 
 }
