631de460a1d46a69670a931471fe3c8f10207444
Draho <drahoslav@yo2.cz>
2021-04-10 12:09:33 +0200

    Update raspberry files

--- raspberrypi/connect-midi.sh
+++ raspberrypi/connect-midi.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/bin/bash --
 
 # put into /usr/local/bin/
 
 # `aconnect -l`
 # to get right values
 
+sleep 1
+
 # connect tablet with piano
 aconnect 24:0 20:0
 aconnect 20:0 24:0
+
 # connect piano with gopiano service
-aconnect 24:0 129:0
+aconnect 20:0 129:0
+
+true
+
