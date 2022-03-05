adf9fb9705df69c786b308787829343b03088da9
Draho <drahoslav@yo2.cz>
2022-03-05 22:12:35 +0100

    Improve chord naming

--- js/midi.js
+++ js/midi.js
@@ -30,43 +30,50 @@ export const fromVal = (val) => val/127
 // eg: [0,3,7] => "Cm"; [2,5,8] =>  "Ddim"
 
 export function nameOfChord(chord) {
-  chord = [...chord].sort((a,b) => a-b)
-	const root = chord[0]
-	const rootNote = ["C", "C#", "D", "D#", "E", "F", "F#" , "G", "G#", "A", "A#", "B"][root]
-	const baseChord = chord.map(n => n-root)
-	const chordEq = (a, b) => String(a) === String(b)
+  const roots = []
+  roots.push(chord[0] % 12) // lowest note played
+  roots.push([...chord].map(n => n % 12).sort()[0]) // lowest note in octave
 
-	if (chordEq(baseChord, [0, 4, 7])) { // major (C, Cmaj) (česky dur)
-		return rootNote
-	}
-	if (chordEq(baseChord, [0, 3, 7])) { // minor (Cm, Cmi, C-) (česky moll)
-		return rootNote + "m"
-	}
-	if (chordEq(baseChord, [0, 3, 6])) { // diminished (Cdim, C°) (česky zmenšená kvinta(?))
-		return rootNote + "dim"
-	}
+  for (let root of roots) {
+    const rootNote = ["C", "C#", "D", "D#", "E", "F", "F#" , "G", "G#", "A", "A#", "B"][root]
+    const baseChord = chord.map(n => ((n+12)-root)%12).sort((a,b) => a-b)
+    const chordEq = (a, b) => String(a) === String(b)
+  
+    // console.log('chord', chord, baseChord)
+  
+    if (chordEq(baseChord, [0, 4, 7])) { // major (C, Cmaj) (česky dur)
+      return rootNote
+    }
+    if (chordEq(baseChord, [0, 3, 7])) { // minor (Cm, Cmi, C-) (česky moll)
+      return rootNote + "m"
+    }
+    if (chordEq(baseChord, [0, 3, 6])) { // diminished (Cdim, C°) (česky zmenšená kvinta(?))
+      return rootNote + "dim"
+    }
+  
+  
+    if (chordEq(baseChord, [0, 4, 7, 9])) { // major sixth (C6)
+      return rootNote + "6"
+    }
+  
+  
+    if (chordEq(baseChord, [0, 4, 7, 11])) { // major seventh (Cmaj7, C∆7)
+      return rootNote + "maj7"
+    }
+    if (chordEq(baseChord, [0, 4, 7, 10])) { // dominant seventh (C7, Cdom7)
+      return rootNote + "7"
+    }
+    if (chordEq(baseChord, [0, 3, 7, 10])) { // minor seventh (Cm7, C-7)
+      return rootNote + "m7"
+    }
+    if (chordEq(baseChord, [0, 3, 5, 9])) { // diminished seventh (Cdim7, C°7)
+      return rootNote + "dim7"
+    }
+  
+    if (chordEq(baseChord, [0, 3, 5, 109])) { // half diminished seventh (Cø, Cm7b)
+      return rootNote + "ø7"
+    }
+  }
 
-
-  if (chordEq(baseChord, [0, 4, 7, 9])) { // major sixth (C6)
-		return rootNote + "6"
-	}
-
-
-	if (chordEq(baseChord, [0, 4, 7, 11])) { // major seventh (Cmaj7, C∆7)
-		return rootNote + "maj7"
-	}
-	if (chordEq(baseChord, [0, 4, 7, 10])) { // dominant seventh (C7, Cdom7)
-		return rootNote + "7"
-	}
-	if (chordEq(baseChord, [0, 3, 7, 10])) { // minor seventh (Cm7, C-7)
-		return rootNote + "m7"
-	}
-	if (chordEq(baseChord, [0, 3, 5, 9])) { // diminished seventh (Cdim7, C°7)
-		return rootNote + "dim7"
-	}
-
-  if (chordEq(baseChord, [0, 3, 5, 109])) { // half diminished seventh (Cø, Cm7b)
-		return rootNote + "ø7"
-	}
 	return ""
 }
